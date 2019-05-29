88df9c42df3944c1165202ec89908ab5221dc60d
Ess-B <51133968+Ess-B@users.noreply.github.com>
2019-05-29 19:59:10 +0300

    Create Leap-Year-Baby.py

---
+++ Leap-Year-Baby.py
@@ -0,0 +1,27 @@
+# By Ashwath from forums
+
+# A leap year baby is a baby born on Feb 29, which occurs only on a leap year.
+
+# Define a procedure is_leap_baby that takes 3 inputs: day, month and year
+# and returns True if the date is a leap day (Feb 29 in a valid leap year)
+# and False otherwise.
+
+# A year that is a multiple of 4 is a leap year unless the year is
+# divisible by 100 but not a multiple of 400 (so, 1900 is not a leap
+# year but 2000 and 2004 are).
+
+def is_leap_baby(day,month,year):
+    # Write your code after this line.
+    if day == 29:
+        if month == 2:
+            if year % 4 != 0:
+                return False
+            elif year % 100 != 0:
+                return True
+            elif year % 400 != 0:
+                return False
+            else:
+                return True
+    else:
+        return False
+                
