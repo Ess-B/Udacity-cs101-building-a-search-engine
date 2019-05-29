ac1e906581e7f8c289bb424e504405dbc9f60718
Ess <51133968+Ess-Bidali@users.noreply.github.com>
2019-05-29 20:53:46 +0300

    Complexity level 2

--- Range-of-a-set.py
+++ Range-of-a-set.py
@@ -2,9 +2,6 @@
 # value. Define a procedure, set_range, which returns the range of three input
 # values.
 
-# Hint: the procedure, biggest which you coded in this unit
-# might help you with this question. You might also like to find a way to
-# code it using some built-in functions.
 
 def set_range(a,b,c):
     # Your code here
